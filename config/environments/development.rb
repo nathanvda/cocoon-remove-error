112f5191504d1dbac146714d514110596079af66
Matheus Mósso <msmosso@osiris.tetra.int>
2014-08-29 12:51:20 -0300

    Final Adjusts.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Cocoon::Application.configure do do
+Cocoon::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
